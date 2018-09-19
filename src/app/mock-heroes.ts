b3115d8827fb72af5eb3885515ca2ce086f76f9c
parkersmathers <parker.smathers@gmail.com>
2018-09-19 17:48:46 -0400

    Create mock heroes and display list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'David Bowie' },
+  { id: 12, name: 'Brian Eno' },
+  { id: 13, name: 'Robert Fripp' },
+  { id: 14, name: 'Tony Visconti' },
+  { id: 15, name: 'Carlos Alomar' },
+  { id: 16, name: 'Colin Thurston' },
+  { id: 17, name: 'Dennis Davis' },
+  { id: 18, name: 'George Murray' }
+];
