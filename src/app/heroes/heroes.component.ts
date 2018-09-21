08b856ffd5a744f0cb5996e26f35a1203ca3c958
parkersmathers <parker.smathers@gmail.com>
2018-09-21 08:39:55 -0400

    Add click event handler for selecting a hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import { Hero } from "../hero";
+import { Hero } from "../hero";
 import { HEROES } from '../mock-heroes';
 
 @Component({
@@ -10,6 +10,12 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
