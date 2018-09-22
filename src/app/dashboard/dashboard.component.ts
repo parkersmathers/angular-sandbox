94ee0a63215695e3337ede2d3a3d5d5992499ab7
parkersmathers <parker.smathers@gmail.com>
2018-09-22 15:18:08 -0400

    Define heroes array prop

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Hero } from '../hero';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  heroes: Hero[] = [];
 
   constructor() { }
 
