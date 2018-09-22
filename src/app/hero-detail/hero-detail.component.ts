d476815ff3f86c5e58ef56a4d3881cc74c265a6a
parkersmathers <parker.smathers@gmail.com>
2018-09-22 16:51:08 -0400

    go back button

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -29,4 +29,9 @@ export class HeroDetailComponent implements OnInit {
     this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero)
   }
+
+  goBack(): void {
+    this.location.back();
+  }
+
 }
