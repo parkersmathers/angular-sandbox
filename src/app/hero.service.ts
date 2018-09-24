4a14e648cbcda1d8ab7a9e99bb491dda9df04aed
parkersmathers <parker.smathers@gmail.com>
2018-09-24 13:45:41 -0400

    Add searchHeroes to hero service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -71,6 +71,19 @@ export class HeroService {
       )
   }
 
+  searchHeroes (term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // If not search term, return empty array
+      return of([]);
+    }
+    return this.http
+      .get<Hero[]>(`${this.heroesUrl}/?name=${term}`)
+      .pipe(
+        tap(_ => this.log(`found heroes matching "${term}"`)),
+        catchError(this.handleError<Hero[]>('searchHeroes', []))
+      )
+  }
+
   /**
   * Handle Http operation that failed.
   * Let the app continue.
