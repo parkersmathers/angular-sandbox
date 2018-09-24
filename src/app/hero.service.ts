8d82aa0ec8b0ce64fc111fc88838a806fbf7a43a
parkersmathers <parker.smathers@gmail.com>
2018-09-24 11:42:53 -0400

    Persist changed hero on server

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
 import { MessageService } from './message.service';
-import { catchError, map, tap } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -33,7 +38,16 @@ export class HeroService {
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fetched hero id=${id}`)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
-  )
+    )
+  }
+
+  // PUT: update hero on the server
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, httpOptions)
+      .pipe(
+        tap(_ => this.log(`updated hero id=${id}`)),
+        catchError(this.handleError<any>('updateHero'))
+      )
   }
 
   // Log a HeroService message with MessageService
